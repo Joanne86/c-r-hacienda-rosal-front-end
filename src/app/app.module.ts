dd1007bd7de367acffc22e4bb4efd894666b56b1
joanne86 <lrodriguez52@uan.edu.co>
2020-01-10 12:02:21 -0500

    add logic to upload excel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,44 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './modules/common/login/login.component';
 import { ResidentNewsComponent } from './modules/administrator/resident-news/resident-news.component';
 import { NotifyResidentsComponent } from './modules/administrator/notify-residents/notify-residents.component';
+import { SelectExcelDataComponent } from './modules/administrator/select-excel-data/select-excel-data.component';
+import {RouterModule, Routes} from '@angular/router';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {CommonModule} from '@angular/common';
+import {UploaderModule} from '@syncfusion/ej2-angular-inputs';
+import {GridAllModule} from '@syncfusion/ej2-angular-grids';
+import {CheckBoxModule} from '@syncfusion/ej2-angular-buttons';
+
+import { HttpModule, JsonpModule } from '@angular/http';
+
+import { DialogModule } from '@syncfusion/ej2-angular-popups';
+const routes: Routes = [
+  { path: 'excel', component: SelectExcelDataComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     ResidentNewsComponent,
-    NotifyResidentsComponent
+    NotifyResidentsComponent,
+    SelectExcelDataComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
+    BrowserModule,
+    CheckBoxModule,
+    GridAllModule,
+    UploaderModule,
+    DialogModule,
+    FormsModule,
+    CommonModule,
+    ReactiveFormsModule,
+    HttpModule,
+    JsonpModule,
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, SelectExcelDataComponent]
 })
 export class AppModule { }
